4f0b1688a660202d0f4f0a74f16778cbfdb30bff
Heymer Adrian Angulo Lopez <adrian900v8@gmail.com>
2026-06-04 04:34:03 -0500

    Corregir URL agregando /api en el frontend

--- finca-frontend/js/login.js
+++ finca-frontend/js/login.js
@@ -1,4 +1,4 @@
-const URL_BACKEND = 'https://kazawencas.onrender.com';
+const URL_BACKEND = 'https://kazawencas.onrender.com/api';
 
 // FIX: Actualizado al nuevo ID 'form-login'
 document.getElementById('form-login').addEventListener('submit', async (e) => {
